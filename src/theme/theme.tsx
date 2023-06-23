33360628b68ef8e72027d5cebfab098b3784cebe
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-06-23 07:56:29 -0300

    refactor: identation

--- src/theme/theme.tsx
+++ src/theme/theme.tsx
@@ -30,8 +30,6 @@ fontSize.body = fontSize[0];
 fontSize.bodyExtraLarge = fontSize[1];
 fontSize.displayExtraLarge = fontSize[2];
 
-
-
 export const useTheme: React.FC<IThemeProps> = ({ theme, children }) => (
     <ThemeProvider theme={theme}>{children}</ThemeProvider>
 );
