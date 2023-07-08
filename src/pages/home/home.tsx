b11872ee97357bcafeef2fd0dd9d2b5c0989fd6c
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-07 21:39:41 -0300

    implments callback function button

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,10 +1,10 @@
-import React, { useState, ChangeEvent, useMemo } from 'react';
+import React, { useState, ChangeEvent, useMemo, Fragment, useCallback, useEffect } from 'react';
 import { Input, Text, Button, Row, Column, List, Image, Icon } from '../../components';
 import { ITask } from 'context';
 import { useTaskContext } from '../../context';
 import { Logo } from '../../assets';
 
-const DEFAULT_SECONDS = 20;
+const DEFAULT_SECONDS = 5;
 const SECONDS_MINUTES = 60;
 
 type Stage = 'Ready' | 'Time to Work' | 'Paused' | 'Stopped' | 'Done';
@@ -36,32 +36,41 @@ export const Home = () => {
     setTaskName('');
   };
 
-  const handleStageTimer = useMemo(() => {
+  const switchCaseStage = (stage: Stage) => {
     switch (stage) {
       case 'Ready':
-        setStage('Ready');
         return 'Ready';
       case 'Time to Work':
-        setStage('Time to Work');
         return 'Time to Work';
       case 'Paused':
-        setStage('Paused');
         return 'Paused';
       case 'Stopped':
-        setStage('Stopped');
         return 'Stopped';
       case 'Done':
-        setStage('Done');
         return 'Done';
+      default:
+        break;
     }
+  };
+
+  const handleStageStatus = useMemo(() => {
+    return switchCaseStage(stage);
   }, [stage]);
 
-  const handleStartTimer = () => {
+  const clearTimerInterval = () => {
+    setTimer((prevTimer: any) => {
+      clearInterval(prevTimer);
+      return undefined;
+    });
+  };
+
+  const handleStartTimer = useCallback(() => {
     setStage('Time to Work');
     const timerInterval = setInterval(() => {
       setSeconds((prevSeconds) => {
         if (prevSeconds === 0) {
           clearInterval(timer);
+          setTimer(undefined);
           setStage('Done');
           return 0;
         }
@@ -70,82 +79,141 @@ export const Home = () => {
     }, 1000);
 
     setTimer(timerInterval);
-  };
+  }, [timer]);
 
-  const handlePauseButton = () => {
+  const handlePauseButton = useCallback(() => {
     setStage('Paused');
-    setTimer((prevTimer: any) => {
-      clearInterval(prevTimer);
-      return undefined;
-    });
-  };
+    clearTimerInterval();
+  }, [timer]);
 
-  const handleStopButton = () => {
-    setStage('Stopped');
-    handlePauseButton();
+  const handleRestartButton = useCallback(() => {
+    setStage('Ready');
+    clearTimerInterval();
     setSeconds(DEFAULT_SECONDS);
-  };
+  }, [timer]);
+
+  const handleStopButton = useCallback(() => {
+    setStage('Stopped');
+    clearTimerInterval();
+    setSeconds(DEFAULT_SECONDS);
+  }, [timer]);
+
+  const handleStageButtons = useMemo(() => {
+    switch (stage) {
+      case 'Ready':
+        return (
+          <Fragment>
+            <Button variant="transparent" width="100%" onClick={handleStartTimer}>
+              <Text color="tertriary" fontWeight="bold" fontSize={20}>
+                Iniciar
+              </Text>
+            </Button>
+          </Fragment>
+        );
+      case 'Time to Work':
+        return (
+          <Fragment>
+            <Button variant="transparent" width="100%" margin={2} onClick={handlePauseButton}>
+              <Icon variant="play" />
+            </Button>
+            <Button variant="transparent" width="100%" margin={2} onClick={handlePauseButton}>
+              <Icon variant="pause" />
+            </Button>
+            <Button variant="transparent" width="100%" margin={2} onClick={handleStopButton}>
+              <Icon variant="stop" />
+            </Button>
+          </Fragment>
+        );
+      case 'Done':
+        return (
+          <Fragment>
+            <Button variant="transparent" width="100%" margin={2} onClick={handleRestartButton}>
+              {''}
+              <Icon variant="restart" />
+            </Button>
+            <Button variant="transparent" width="100%" margin={2}>
+              {''}
+              <Icon variant="done" />
+            </Button>
+          </Fragment>
+        );
+      case 'Paused':
+        return (
+          <Fragment>
+            <Button variant="transparent" width="100%" onClick={handleStartTimer}>
+              <Text color="tertriary" fontWeight="bold" fontSize={20}>
+                Retornar
+              </Text>
+            </Button>
+          </Fragment>
+        );
+      case 'Stopped':
+        return (
+          <Fragment>
+            <Button variant="transparent" width="100%" onClick={handleStartTimer}>
+              <Text color="tertriary" fontWeight="bold" fontSize={20}>
+                Iniciar
+              </Text>
+            </Button>
+          </Fragment>
+        );
+      default:
+        return (
+          <Fragment>
+            <Button variant="transparent" width="100%" onClick={handleStartTimer}>
+              <Text color="tertriary" fontWeight="bold" fontSize={20}>
+                Iniciar
+              </Text>
+            </Button>
+          </Fragment>
+        );
+    }
+  }, [handlePauseButton, handleStopButton, handleStartTimer, stage]);
 
   const listItems = taskList.map((task) => ({ label: task.name }));
 
+  useEffect(() => {
+    return () => {
+      if (timer) return clearTimeout(timer);
+    };
+  }, [timer]);
+
   return (
-    <React.Fragment>
-      <Column width="50%" margin="0 auto">
-        <Column
-          width="100%"
-          display="flex"
-          borderRadius={10}
-          marginBottom={4}
-          marginTop={4}
-          textAlign="center"
-          bg="rgba(255,255,255,0.2)"
-          padding={4}
-          alignItems="center">
-          <Image src={Logo} width="30%" />
-          <Text color="tertiary" fontWeight="bold" fontSize={20}>
-            {handleStageTimer}
-          </Text>
-          <Text color="tertiary" fontWeight="bold" fontSize={80} property="30px">
-            {secondsToTimer(seconds)}
-          </Text>
-          <Button variant="transparent" width="53%" onClick={handleStartTimer}>
-            <Text color="tertriary" fontWeight="bold" fontSize={20}>
-              Iniciar
-            </Text>
-          </Button>
-          <Row>
-            <Button variant="transparent" width="25%" margin={2} onClick={handleStartTimer}>
-              <Icon variant="play" />
-            </Button>
-            <Button variant="transparent" width="25%" margin={2} onClick={handlePauseButton}>
-              <Icon variant="pause" />
-            </Button>
-            <Button variant="transparent" width="25%" margin={2}>
-              <Icon variant="restart" />
-            </Button>
-            <Button variant="transparent" width="25%" margin={2} onClick={handleStopButton}>
-              <Icon variant="stop" />
-            </Button>
-            <Button variant="transparent" width="25%" margin={2}>
-              <Icon variant="done" />
-            </Button>
-          </Row>
-        </Column>
-        <Column textAlign="center">
-          <Text color="secondary" fontWeight="bold" fontSize={40}>
-            Tarefas
-          </Text>
-        </Column>
-        <Row width="100%">
-          <Input flex={1} placeholder="Adicionar tarefa" value={taskName} onChange={handleInputChange} />
-          <Button onClick={handleAddTask}>
-            <Text color="secondary" fontWeight="bold" fontSize={1}>
-              Adicionar
-            </Text>
-          </Button>
-        </Row>
-        <List items={listItems} />
+    <Column width="50%" margin="0 auto">
+      <Column
+        width="100%"
+        display="flex"
+        borderRadius={10}
+        marginBottom={4}
+        marginTop={4}
+        textAlign="center"
+        bg="rgba(255,255,255,0.2)"
+        padding={4}
+        alignItems="center">
+        <Image src={Logo} width="30%" />
+        <Text color="tertiary" fontWeight="bold" fontSize={20}>
+          {handleStageStatus}
+        </Text>
+        <Text color="tertiary" fontWeight="bold" fontSize={80} property="30px">
+          {secondsToTimer(seconds)}
+        </Text>
+
+        <Row>{handleStageButtons}</Row>
       </Column>
-    </React.Fragment>
+      <Column textAlign="center">
+        <Text color="secondary" fontWeight="bold" fontSize={40}>
+          Tarefas
+        </Text>
+      </Column>
+      <Row width="100%">
+        <Input flex={1} placeholder="Adicionar tarefa" value={taskName} onChange={handleInputChange} />
+        <Button onClick={handleAddTask}>
+          <Text color="secondary" fontWeight="bold" fontSize={1}>
+            Adicionar
+          </Text>
+        </Button>
+      </Row>
+      <List items={listItems} />
+    </Column>
   );
 };
